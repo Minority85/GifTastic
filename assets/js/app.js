67746a42ec12e12ccc88a435e8bad0b29ddd0d69
Daniel Bryant <azbryant85@gmail.com>
2019-03-19 21:20:28 -0700

    2nd update

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ function gifQuery() {
 
     var searchAnimal = $(this).attr("data-name");
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchAnimal + "&api_key=" + apiKey + "&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchAnimal + "&api_key=" + apiKey + "&limit=10"
 
     $.ajax({
         url: queryURL,
